599aee8379326d64a1afb3a266e1ca13030456d9
safat <safatcsedu@gmail.com>
2014-07-01 15:07:01 +0600

    jsf sample loop added

--- ressearch-content-manger-war/src/main/java/net/therap/action/HomeAction.java
+++ ressearch-content-manger-war/src/main/java/net/therap/action/HomeAction.java
@@ -1,5 +1,6 @@
 package net.therap.action;
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
 import java.util.Arrays;
@@ -8,11 +9,12 @@ import java.util.List;
 /**
  * Created by shakhawat.hossain on 7/1/14.
  */
+
 @ManagedBean (name = "homeAction", eager = true)
+@ApplicationScoped
 public class HomeAction implements Serializable {
 
     public List<String> getResearchTopicList(){
         return Arrays.asList("data mining", "networking", "machine -learning");
     }
-
 }
